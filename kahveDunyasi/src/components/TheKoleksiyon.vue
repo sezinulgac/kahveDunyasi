e9d2fbaf791d6298a001bc8479d55a3e238c72fb
Cansu KAHRAMAN <130057854+cansu-kahraman@users.noreply.github.com>
2023-12-13 14:52:04 +0300

    the koleksiyon added

---
+++ kahveDunyasi/src/components/TheKoleksiyon.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <div class="col image q-pa-md">
+      <a href="https://www.kahvedunyasi.com/hediye-gonder" class>
+        <img src="https://img-kahvedunyasi.mncdn.com/mncropresize/748/473/kahvedunyasi/static/img/campaign/hediye-v3.jpg">
+      </a>
+    </div>
+
+    <q-seperator></q-seperator>
+
+    <div class="col image q-pa-md">
+      <a href="https://www.kahvedunyasi.com/sosyal-sorumluluk-cl-108" class>
+        <img src="https://img-kahvedunyasi.mncdn.com/mncropresize/748/473/kahvedunyasi/static/img/campaign/darussafaka.jpg?v=3">
+      </a>
+    </div>
+    
+   
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style scoped>
+.image {
+  width: 100%;
+  height: auto; 
+  max-width: 100%; 
+  display: block; 
+  margin-bottom: 10px; 
+}
+</style>
